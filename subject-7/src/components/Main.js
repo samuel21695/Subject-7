79ce3f8f20fcaa5d0131f0f9aab6b02cdd4614cf
samuel21695 <samuel21695@gmail.com>
2023-12-29 16:35:10 +0900

    ✨ import Sidebar component

---
+++ subject-7/src/components/Main.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import Sidebar from "./Sidebar";
+
+const Main = () =>{
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Main;
