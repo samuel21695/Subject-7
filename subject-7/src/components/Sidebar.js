55d7fe0ec6137f96b1a0a1e0eb34f0e38a3b37fc
samuel21695 <samuel21695@gmail.com>
2023-12-29 16:28:29 +0900

    📄 create Sidebar component

---
+++ subject-7/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <ul>
+      <li><a href="#home">김우진</a></li>
+      <li><a href="#hyun">김현</a></li>
+      <li><a href="#seunghee">방승희</a></li>
+      <li><a href="#honyeong">변호녕</a></li>
+      <li><a href="#samuel">소사무엘</a></li>
+      <li><a href="#youngjoon">송영준</a></li>
+      <li><a href="#donghyun">신동현</a></li>
+      <li><a href="#seungmin">오승민</a></li>
+      <li><a href="#seungmin2">유승민</a></li>
+      <li><a href="#joon">윤준현</a></li>
+      <li><a href="#mingoo">이민구</a></li>
+      <li><a href="#yuan">이유안</a></li>
+      <li><a href="#chae">이채이</a></li>
+      <li><a href="#young">정영식</a></li>
+    </ul>
+  );
+};
+
+export default Sidebar;
