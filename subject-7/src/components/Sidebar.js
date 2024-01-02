58d0c4d3390a2a048e4af143ea5d5cdd00a00035
samuel21695 <samuel21695@gmail.com>
2024-01-02 19:59:46 +0900

    ✨logic align

--- subject-7/src/components/Sidebar.js
+++ subject-7/src/components/Sidebar.js
@@ -1,5 +1,29 @@
 import React from 'react';
 
+
+const Sidebar = () => {
+  return (
+    <div id="sidebar">
+      <ul>
+        <li><a href="#home">김우진</a></li>
+        <li><a href="#hyun">김현</a></li>
+        <li><a href="#seunghee">방승희</a></li>
+        <li><a href="#honyeong">변호녕</a></li>
+        <li><a href="#samuel">소사무엘</a></li>
+        <li><a href="#youngjoon">송영준</a></li>
+        <li><a href="#donghyun">신동현</a></li>
+        <li><a href="#seungmin">오승민</a></li>
+        <li><a href="#seungmin2">유승민</a></li>
+        <li><a href="#joon">윤준현</a></li>
+        <li><a href="#mingoo">이민구</a></li>
+        <li><a href="#yuan">이유안</a></li>
+        <li><a href="#chae">이채이</a></li>
+        <li><a href="#young">정영식</a></li>
+      </ul>
+    </div>
+  );
+};
+
 function navigatePage(page) {
   switch (page) {
     case 'home':
@@ -50,40 +74,25 @@ function navigatePage(page) {
   }
 }
 
-  // 초기 페이지 로딩 시 홈 페이지 표시
-  navigatePage('home');
+// 각 링크 클릭 시 페이지 이동
+const aTags = document.querySelectorAll('ul a')
+aTags.forEach(link => {
+  link.addEventListener('click', () => {
+    const page = link.getAttribute('href').substring(1); // # 제거
+    navigatePage(page);
+  });
+});  
 
-  // 각 링크 클릭 시 페이지 이동
-  const aTags = document.querySelectorAll('ul a')
+// form 제출 시 이벤트 발생
+document.getElementById('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputValue = document.getElementById('text').value
   aTags.forEach(link => {
-    link.addEventListener('click', () => {
-      const page = link.getAttribute('href').substring(1); // # 제거
-      navigatePage(page);
-    });
-  });  
-
-
-const Sidebar = () => {
-  return (
-    <div id="sidebar">
-      <ul>
-        <li><a href="#home">김우진</a></li>
-        <li><a href="#hyun">김현</a></li>
-        <li><a href="#seunghee">방승희</a></li>
-        <li><a href="#honyeong">변호녕</a></li>
-        <li><a href="#samuel">소사무엘</a></li>
-        <li><a href="#youngjoon">송영준</a></li>
-        <li><a href="#donghyun">신동현</a></li>
-        <li><a href="#seungmin">오승민</a></li>
-        <li><a href="#seungmin2">유승민</a></li>
-        <li><a href="#joon">윤준현</a></li>
-        <li><a href="#mingoo">이민구</a></li>
-        <li><a href="#yuan">이유안</a></li>
-        <li><a href="#chae">이채이</a></li>
-        <li><a href="#young">정영식</a></li>
-      </ul>
-    </div>
-  );
-};
+    if (link.textContent === inputValue) {
+    const page = link.getAttribute('href').substring(1); // # 제거
+    navigatePage(page);
+  };
+});
+});
 
 export default Sidebar;
