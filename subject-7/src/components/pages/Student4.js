03f143e1033fe7f57b3fceeefed06f5110bf155b
samuel21695 <samuel21695@gmail.com>
2024-01-08 01:01:12 +0900

    📄  multiple pages

---
+++ subject-7/src/components/pages/Student4.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// 페이지 컴포넌트들
+const StudentFour = () => (
+  <div>
+    <div className="introduce">
+      <h1>변호녕 소개</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium enim placeat fugit mollitia error totam voluptatibus, corrupti tempore? Illo libero, eveniet dolorum nihil nemo incidunt aut ea maxime necessitatibus odio.</p>
+    </div>
+    <div className="character">
+      <h2>변호녕 장점</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga voluptas, facilis omnis modi dolores rem nobis atque nesciunt dolorum iure? Impedit esse modi quo facere rem adipisci ad tempora accusamus?</p>
+    </div>
+  </div>
+);
+
+export default StudentFour
